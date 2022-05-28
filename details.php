c2d71743a769cf85493eea9200aaa7225eb90073
maidesu <vorpeti@gmail.com>
2022-05-28 23:02:47 +0200

    Add main page

---
+++ details.php
@@ -0,0 +1,1 @@
+;details based on get id
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
     include('seriesstorage.php');
+
+    $series_storage = new SeriesStorage();
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +11,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>(not) my anime list</title>
+    <title>(Not) my anime list</title>
 </head>
 <body>
+    <div>
+        <h1>(Nem) Az Én Anime Listám</h1>
+        <p>Ez nem egy az 'Az Én Anime Listám' mintájára készült anime sorozatkövető weboldal.</p>
+
+        <table>
+            <tr>
+                <th>Borító</th>
+                <th>Cím</th>
+                <th>Epizódok száma</th>
+                <th>Legutóbbi epizód dátuma</th>
+            </tr>
+            <?php foreach ($series_storage->findAll()["series"] as $listing): ?>
+                <tr>
+                    <td><?= $listing["cover"] ?></td>
+                    <td><a href="./details.php?id=<?= $listing["id"] ?>"><?= $listing["title"] ?></a></td>
+                    <td><?= count($listing["episodes"]) ?></td>
+                    <td><?= $listing["episodes"][count($listing["episodes"])]["date"] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
+
+    <!-- PHP BEJELENTKEZETT FELHASZNÁLÓ LEGALÁBB EGY MEGTEKINTETT LISTA -->
+
+    <!-- PHP CSAK ADMIN FORM HOZZÁADÁSHOZ -->
     <form action="" novalidate>
     </form>
 </body>
