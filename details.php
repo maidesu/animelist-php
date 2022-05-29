e6e9eb403bfb2a3e8a1faa9bba83fb010035bcbb
maidesu <vorpeti@gmail.com>
2022-05-30 00:44:37 +0200

    Ability to add episodes to anime

--- details.php
+++ details.php
@@ -12,6 +12,60 @@
         $_SESSION["user"]["watched"][$_GET["id"]] = $_GET["viewed"];
         $user_storage->update($_SESSION["user"]["id"], $_SESSION["user"]);
     }
+
+
+    function redirect($page) {
+        header("Location: ${page}");
+        exit();
+    }
+
+    function validate($post, &$data, &$errors) {
+        if (!isset($post["id"]) || $post["id"] == "") $errors['id'] = "Nincs megadva ID!";
+
+        if (!isset($post["title"]) || $post["title"] == "") $errors['title'] = "Nincs megadva cím!";
+
+        if (!isset($post["date"]) || $post["date"] == "") $errors['date'] = "Nincs megadva dátum!";
+
+        if (!isset($post["plot"]) || $post["plot"] == "") $errors['plot'] = "Nincs megadva leírás!";
+
+        if (!isset($post["rating"]) || $post["rating"] == "") $errors['rating'] = "Nincs megadva értékelés!";
+        else if (!is_float($post["rating"] + 0) || $post["rating"] < 0 || $post["rating"] > 10) $errors['rating'] = "Az értékelés 0 és 10 közti lebegőpontos szám legyen!";
+
+        $data = $post;
+        return count($errors) === 0;
+    }
+
+    function episode_exists($series_storage, $title) {
+        $series = $series_storage->findById($_GET["id"]);
+        return in_array($title, $series["episodes"]);
+    }
+
+    function add_episode($series_storage, $data) {
+        $series = $series_storage->findById($_GET["id"]);
+        $episode = [
+            "id" => $data['id'],
+            "date" => $data['date'],
+            "title" => $data['title'],
+            "plot" => $data['plot'],
+            "rating" => $data['rating']
+        ];
+        $series["episodes"][$episode["id"]] = $episode;
+
+        return $series_storage->update($_GET["id"], $series);
+    }
+
+    $errors = [];
+    $data = [];
+    if (count($_POST) > 0) {
+      if (validate($_POST, $data, $errors)) {
+          if (episode_exists($series_storage, $data['title'])) {
+            $errors['global'] = "Episode already exists";
+          } else {
+            add_episode($series_storage, $data);
+            header("Refresh:0");
+          }
+      }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -77,6 +131,46 @@
                         ($i === 1 && !isset($_SESSION["user"]["watched"][$_GET["id"]]))) :?><td><a href="./details.php?id=<?= $series["id"] ?>&viewed=<?= $i ?>">+</a></td><?php endif; ?>
                 </tr>
             <?php endforeach; ?>
+            <tr>
+                <form action="" method="post" novalidate>
+                    <td>
+                        <input type="text" name="id" id="id" value="<?= $_POST['id'] ?? "" ?>">
+                        <?php if (isset($errors['id'])) : ?>
+                            <span class="error"><?= $errors['id'] ?></span>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <input type="text" name="title" id="title" value="<?= $_POST['title'] ?? "" ?>">
+                        <?php if (isset($errors['title'])) : ?>
+                            <span class="error"><?= $errors['title'] ?></span>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <input type="text" name="date" id="date" value="<?= $_POST['date'] ?? "" ?>">
+                        <?php if (isset($errors['date'])) : ?>
+                            <span class="error"><?= $errors['date'] ?></span>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <textarea name="plot" id="plot"><?= $_POST['plot'] ?? "" ?></textarea>
+                        <?php if (isset($errors['plot'])) : ?>
+                            <span class="plot"><?= $errors['plot'] ?></span>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <input type="text" name="rating" id="rating" value="<?= $_POST['rating'] ?? "" ?>">
+                        <?php if (isset($errors['rating'])) : ?>
+                            <span class="error"><?= $errors['rating'] ?></span>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <button type="submit">Hozzáadás</button>
+                    </td>
+                </form>
+                <?php if (isset($errors['global'])) : ?>
+                    <p><span class="error"><?= $errors['global'] ?></span></p>
+                <?php endif; ?>
+            </tr>
         </table>
     </div>
 </body>
