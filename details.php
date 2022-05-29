maidesu/animelist-php

commit cc483935644d8beb9c21f25ba3485d9fd1a442c9

- Ability to add anime episode to watched - Users watched some more anime

details.php
@@ -1,11 +1,17 @@
 <?php
-    include('seriesstorage.php');
+    include('filestorage.php');
 
     session_start();
     $series_storage = new SeriesStorage();
+    $user_storage = new UserStorage();
 
     if (!isset($_GET["id"]) || $_GET["id"] === "" || is_null($series_storage->findById($_GET["id"]))) { http_response_code(404); return; }
     $series = $series_storage->findById($_GET["id"]);
+
+    if (isset($_GET["viewed"]) && is_numeric($_GET["viewed"])) {
+        $_SESSION["user"]["watched"][$_GET["id"]] = $_GET["viewed"];
+        $user_storage->update($_SESSION["user"]["id"], $_SESSION["user"]);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -59,7 +65,10 @@
                     <td><?= $details["date"] ?></td>
                     <td><?= $details["plot"] ?></td>
                     <td><?= $details["rating"] ?></td>
-                    <?php if (isset($_SESSION["user"])) :?><td><a href="./details.php?id=<?= $series["id"] ?>&viewed=<?= $i ?>">+</a></td><?php endif; ?>
+                    <?php if (isset($_SESSION["user"]) &&
+                        isset($_SESSION["user"]["watched"][$_GET["id"]]) &&
+                        $i === $_SESSION["user"]["watched"][$_GET["id"]] + 1 ||
+                        ($i === 1 && !isset($_SESSION["user"]["watched"][$_GET["id"]]))) :?><td><a href="./details.php?id=<?= $series["id"] ?>&viewed=<?= $i ?>">+</a></td><?php endif; ?>
                 </tr>
             <?php endforeach; ?>
         </table>

filestorage.php
@@ -6,4 +6,10 @@ class SeriesStorage extends Storage {
     parent::__construct(new JsonIO('series.json'));
   }
 }
+
+class UserStorage extends Storage {
+  public function __construct() {
+    parent::__construct(new JsonIO('user.json'));
+  }
+}
 ?>

index.php
@@ -1,5 +1,5 @@
 <?php
-    include('seriesstorage.php');
+    include('filestorage.php');
 
     session_start();
     $series_storage = new SeriesStorage();

login.php
@@ -1,5 +1,5 @@
 <?php
-    include('userstorage.php');
+    include('filestorage.php');
 
     // functions
     function redirect($page) {

register.php
@@ -1,5 +1,5 @@
 <?php
-    include('userstorage.php');
+    include('filestorage.php');
 
     // functions
     function validate($post, &$data, &$errors) {

commit ba637470597da1e5ad69a90d42ef4e82f2138b4a

cc4839356 introduced bug fixed (adding watched to anonymous user)

details.php
@@ -8,7 +8,7 @@
     if (!isset($_GET["id"]) || $_GET["id"] === "" || is_null($series_storage->findById($_GET["id"]))) { http_response_code(404); return; }
     $series = $series_storage->findById($_GET["id"]);
 
-    if (isset($_GET["viewed"]) && is_numeric($_GET["viewed"])) {
+    if (isset($_SESSION["user"]) && isset($_GET["viewed"]) && is_numeric($_GET["viewed"])) {
         $_SESSION["user"]["watched"][$_GET["id"]] = $_GET["viewed"];
         $user_storage->update($_SESSION["user"]["id"], $_SESSION["user"]);
     }
@@ -125,10 +125,15 @@
                     <td><?= $details["date"] ?></td>
                     <td><?= $details["plot"] ?></td>
                     <td><?= $details["rating"] ?></td>
-                    <?php if (isset($_SESSION["user"]) &&
-                        isset($_SESSION["user"]["watched"][$_GET["id"]]) &&
-                        $i === $_SESSION["user"]["watched"][$_GET["id"]] + 1 ||
-                        ($i === 1 && !isset($_SESSION["user"]["watched"][$_GET["id"]]))) :?><td><a href="./details.php?id=<?= $series["id"] ?>&viewed=<?= $i ?>">+</a></td><?php endif; ?>
+                    <?php
+                        if (isset($_SESSION["user"]) && // Valid user
+                        isset($_SESSION["user"]["watched"][$_GET["id"]]) && // Has the show indexed in user.json
+                        ($i === $_SESSION["user"]["watched"][$_GET["id"]] + 1) || // Place button next to first unwatched episode
+                        (isset($_SESSION["user"]) && !isset($_SESSION["user"]["watched"][$_GET["id"]]) && $i === 1) // User is valid, but hasn't indexed show yet -> button to first episode
+                        ) :
+                    ?>
+                        <td><a href="./details.php?id=<?= $series["id"] ?>&viewed=<?= $i ?>">+</a></td>
+                    <?php endif; ?>
                 </tr>
             <?php endforeach; ?>
             <?php if (isset($_SESSION["user"]["isAdmin"]) && $_SESSION["user"]["isAdmin"] === 1) :?>